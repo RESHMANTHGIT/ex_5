4c6a901d3b641abb3d3a4cf3b36bdc44a3e1a746
Kishore4123 <76401205+Kishore4123@users.noreply.github.com>
2025-03-18 16:09:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+$("h1").addClass("big-title margin-prop");
+$("h1").html("<em>HELLO</em>")
+$("button").text("Click On me :")
+$("h1").click(function(){
+    $("h1").css("color","purple")
+})
+
+$("button").click(function(){
+    $("h1").css("color","purple")
+})
+
+$("input")
